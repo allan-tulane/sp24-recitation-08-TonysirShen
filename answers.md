32f472a67dee59549fa6f82585b557d5f24b609b
TonysirShen <142557086+TonysirShen@users.noreply.github.com>
2024-04-24 01:56:03 +0000

    finish lab8

--- answers.md
+++ answers.md
@@ -2,17 +2,27 @@
 
 ## Answers
 
-**Name:**_________________________
-**Name:**_________________________
+**Name:**_____Sean Hall____________________
+**Name:**______________Tony Shen___________
 
 
 Place all written answers from `recitation-08.md` here for easier grading.
 
 
 
-- **1b)**
+- **1b)** We call this function once per edge, since a node may be added multiple times for each edge. Thus the work is O(E log E)
 
+Because this algorithm must be completed sequentially, the span must also be O(E log E)
+        - Work:  O(E log E)
+          Span: O(E log E)
 
 
 - **2b)**
-
+- def get_path(parents, destination):
+  if destination not in parents:
+    return ''
+  else:
+    parent = parents[destination]
+    return get_path(parents, parent) + parent
+This recursive function checks the destination's parent and prepend the parent's prentn's to it until there is no parent.
+Its work and span are both O(N)
--- main.py
+++ main.py
@@ -1,48 +1,73 @@
 from collections import deque
-from heapq import heappush, heappop 
+from heapq import heappush, heappop
+
 
 def shortest_shortest_path(graph, source):
-    """
-    Params: 
-      graph.....a graph represented as a dict where each key is a vertex
-                and the value is a set of (vertex, weight) tuples (as in the test case)
-      source....the source node
-      
-    Returns:
-      a dict where each key is a vertex and the value is a tuple of
-      (shortest path weight, shortest path number of edges). See test case for example.
-    """
-    ### TODO
-    pass
-    
 
-    
-    
+  def dijkstra_helper(visited, frontier):
+    while frontier:
+      distance, edges, node = heappop(frontier)  ### get current
+      if node not in visited:
+        visited[node] = (distance, edges)
+        for neighbor, weight in graph[node]:  ### visting neighbours
+          edges += 1
+          distance += weight
+          heappush(frontier, (distance, edges, neighbor))
+    return visited
+
+  frontier = []
+  heappush(frontier, (0, 0, source))
+  visited = {}
+  return dijkstra_helper(visited, frontier)
+
+
 def bfs_path(graph, source):
-    """
-    Returns:
-      a dict where each key is a vertex and the value is the parent of 
-      that vertex in the shortest path tree.
-    """
-    ###TODO
-    pass
+
+  def bfs_helper(visited, frontier, parents):
+    while frontier:
+      current = frontier.pop()
+      visited.add(current)
+      children = graph[current]
+      for node in children:
+        if node not in parent:
+          parents[node] = current
+        if node not in visited:
+          frontier.append(node)
+    return parents
+
+  visited = set()
+  frontier = deque()
+  parent = {}
+  frontier.append(source)
+  return bfs_helper(visited, frontier, parent)
+
+
+graph = {'s': {'a', 'b'}, 'a': {'b'}, 'b': {'c'}, 'c': {'a', 'd'}, 'd': {}}
+print(bfs_path(graph, 's'))
+
 
 def get_sample_graph():
-     return {'s': {'a', 'b'},
-            'a': {'b'},
-            'b': {'c'},
-            'c': {'a', 'd'},
-            'd': {}
-            }
+  return {'s': {'a', 'b'}, 'a': {'b'}, 'b': {'c'}, 'c': {'a', 'd'}, 'd': {}}
 
 
-    
 def get_path(parents, destination):
-    """
-    Returns:
-      The shortest path from the source node to this destination node 
-      (excluding the destination node itself). See test_get_path for an example.
-    """
-    ###TODO
-    pass
+  if destination not in parents:
+    return ''
+  else:
+    parent = parents[destination]
+    return get_path(parents, parent) + parent
 
+
+print(get_path(bfs_path(graph, 's'), 'd'))
+
+graph = {
+    's': {('a', 1), ('c', 4)},
+    'a': {('b', 2)},  # 'a': {'b'},
+    'b': {('c', 1), ('d', 4)},
+    'c': {('d', 3)},
+    'd': {},
+    'e': {('d', 0)}
+}
+
+result = shortest_shortest_path(graph, 's')
+print(result)
